bae94b049f68345855da69a0df8945add344e9a3
kacper <kacper.babka@gmail.com>
2023-07-20 17:56:38 +0200

    Columnt + Image in the center

--- app/src/main/java/com/example/buissnesscardapp/MainActivity.kt
+++ app/src/main/java/com/example/buissnesscardapp/MainActivity.kt
@@ -3,13 +3,26 @@ package com.example.buissnesscardapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.buissnesscardapp.ui.theme.BuissnessCardAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +35,6 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
                 }
             }
         }
@@ -30,17 +42,45 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun BusinessCard(){
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        val image = painterResource(id = R.drawable.android_logo)
+
+        Image(
+            painter = image,
+            contentDescription = null,
+            Modifier.fillMaxWidth(0.35f).background(Color(0xFF073042))
+        )
+    }
+}
+
+@Composable
+fun ContactRow(text: String, icon: ImageVector) {
+    Row(
+        horizontalArrangement = Arrangement.Center,
+        modifier = Modifier.padding(16.dp)
+    ) {
+        Icon(
+            imageVector = icon,
+            contentDescription = null,
+            tint = Color.Green
+        )
+        Text(
+            text = text,
+            color = Color.Black
+            )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    BuissnessCardAppTheme {
-        Greeting("Android")
+    Surface(
+        modifier = Modifier.fillMaxSize(), color = Color(0xFFd2e8d4)
+    ){
+        BusinessCard()
     }
 }
