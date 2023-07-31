4975af789979e54171766d647022d84f3de52167
kacper <kacper.babka@gmail.com>
2023-07-31 12:08:26 +0200

    ContactRows added

--- app/src/main/java/com/example/buissnesscardapp/MainActivity.kt
+++ app/src/main/java/com/example/buissnesscardapp/MainActivity.kt
@@ -11,8 +11,11 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Email
+import androidx.compose.material.icons.rounded.Phone
+import androidx.compose.material.icons.rounded.Share
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -21,8 +24,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.buissnesscardapp.ui.theme.BuissnessCardAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -30,11 +36,10 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             BuissnessCardAppTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    modifier = Modifier.fillMaxSize(), color = Color(0xFFd2e8d4)
                 ) {
+                    BusinessCard()
                 }
             }
         }
@@ -42,17 +47,52 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun BusinessCard(){
+fun BusinessCard() {
     Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+        horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center
     ) {
         val image = painterResource(id = R.drawable.android_logo)
 
         Image(
             painter = image,
             contentDescription = null,
-            Modifier.fillMaxWidth(0.35f).background(Color(0xFF073042))
+            Modifier
+                .fillMaxWidth(0.35f)
+                .background(Color(0xFF073042))
+        )
+        Text(
+            text = stringResource(R.string.my_name),
+            fontSize = 28.sp,
+            modifier = Modifier
+                .padding(top = 5.dp)
+        )
+        Text(
+            text = stringResource(R.string.android_developer_extraordinaire),
+            color = Color(0xFF006c39),
+            fontSize = 15.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .padding(top = 5.dp)
+        )
+    }
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(bottom = 32.dp),
+        verticalArrangement = Arrangement.Bottom,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        ContactRow(
+            text = stringResource(R.string.phone_number),
+            icon = Icons.Rounded.Phone
+        )
+        ContactRow(
+            text = stringResource(R.string.share),
+            icon = Icons.Rounded.Share
+        )
+        ContactRow(
+            text = stringResource(R.string.kacper_blabla_gmail_com),
+            icon = Icons.Rounded.Email
         )
     }
 }
@@ -60,18 +100,21 @@ fun BusinessCard(){
 @Composable
 fun ContactRow(text: String, icon: ImageVector) {
     Row(
-        horizontalArrangement = Arrangement.Center,
-        modifier = Modifier.padding(16.dp)
+        modifier = Modifier.padding(8.dp)
     ) {
         Icon(
             imageVector = icon,
             contentDescription = null,
-            tint = Color.Green
+            tint = Color(0xFF006c39),
+            modifier = Modifier
+                .weight(2f)
         )
         Text(
             text = text,
-            color = Color.Black
-            )
+            color = Color.Black,
+            modifier = Modifier
+                .weight(3f)
+        )
     }
 }
 
@@ -80,7 +123,7 @@ fun ContactRow(text: String, icon: ImageVector) {
 fun GreetingPreview() {
     Surface(
         modifier = Modifier.fillMaxSize(), color = Color(0xFFd2e8d4)
-    ){
+    ) {
         BusinessCard()
     }
 }
